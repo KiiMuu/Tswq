62f3a775bb1505f97cccab71b39fe5cae854f564
Karim MuHamad <karimmuhamadfci@gmail.com>
2021-01-11 05:50:27 +0200

    Basic express sever

---
+++ api/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.send('API is on fire!')
+});
+
+app.get('/products', (req, res) => {
+    res.json(products)
+});
+
+app.get('/products/:id', (req, res) => {
+    const pro = products.find(p => p._id === req.params.id);
+
+    res.json(pro)
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`App is up on port ${port}`));
