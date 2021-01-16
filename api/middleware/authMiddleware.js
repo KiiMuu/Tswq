1476a79dbe400f4a2fde2504f08b952916b134a9
Karim MuHamad <karimmuhamadfci@gmail.com>
2021-01-16 02:13:45 +0200

    custom auth middleware

---
+++ api/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const protectRoute = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+
+            // decoded => { id: '123456', iat: 3213123, exp: 3213432 }
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (err) {
+            console.error(err);
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
+export { protectRoute }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 // controllers
 import {
-    authUser
+    authUser,
+    getUserProfile
 } from '../controllers/userController.js';
+
+// middlewarea
+import { protectRoute } from '../middleware/authMiddleware.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/signin
 // @access  Public
 router.post('/signin', authUser);
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+router.get('/profile', protectRoute, getUserProfile);
 
 export default router;
