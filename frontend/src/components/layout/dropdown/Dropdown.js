05922148950cfa6c548a917ba10edc634fc86dbf
Karim MuHamad <karimmuhamadfci@gmail.com>
2021-01-21 05:01:25 +0200

    hide dropdown on click

--- frontend/src/components/layout/dropdown/Dropdown.js
+++ frontend/src/components/layout/dropdown/Dropdown.js
@@ -2,26 +2,29 @@ import styles from './Dropdown.module.scss';
 import useToggle from '../../../hooks/useToggle';
 
 const Dropdown = ({ name, icon, items, style }) => {
-    const { isOpen, handleToggle, elementRef } = useToggle();
+	const { isOpen, handleToggle, elementRef } = useToggle();
 
-    const options = items.map(item => (
-        <li key={item.id} className={styles.item}>{item.content}</li>
-    ));
+	const options = items.map((item) => (
+		<li key={item.id} className={styles.item} onClick={handleToggle}>
+			{item.content}
+		</li>
+	));
 
-    return (
-        <div className={styles.dropdown}>
-            <button onClick={handleToggle} style={style}>
-                {name} <span>{icon}</span>
-            </button>
-            <nav
-                ref={elementRef} 
-                className={`${styles.dropdownContent} ${isOpen ? styles.open : ''} rounded-md shadow-lg`}>
-                <ul className='list-none'>
-                    {options}
-                </ul>
-            </nav>
-        </div>
-    )
-}
+	return (
+		<div className={styles.dropdown}>
+			<button onClick={handleToggle} style={style}>
+				{name} <span>{icon}</span>
+			</button>
+			<nav
+				ref={elementRef}
+				className={`${styles.dropdownContent} ${
+					isOpen ? styles.open : ''
+				} rounded-md shadow-lg`}
+			>
+				<ul className="list-none">{options}</ul>
+			</nav>
+		</div>
+	);
+};
 
 export default Dropdown;
